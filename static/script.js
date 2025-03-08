5fea94e03e22d73b6094bc7cc1c599ec6b7964a3
yeeeengyu <153285299+yeeeeeeeeeeengyu@users.noreply.github.com>
2025-03-08 19:26:11 +0900

    updated ui&server&jsCbalAllShutTheFuckUp

--- static/script.js
+++ static/script.js
@@ -9,6 +9,7 @@ let selectedFile = null;
 
 fileInput.addEventListener("change", (event) => {
   const file = event.target.files[0];
+
   if (file && file.type.startsWith("image/")) {
     const img = document.createElement("img");
     img.src = URL.createObjectURL(file);
@@ -16,32 +17,33 @@ fileInput.addEventListener("change", (event) => {
     img.style.maxWidth = "620px";
     img.style.marginTop = "40px";
 
-    // 기존 이미지 삭제 후 새로운 이미지 추가
     imageContainer.innerHTML = "";
     imageContainer.appendChild(img);
 
-    // 버튼 텍스트 변경
     uploadBtn.textContent = `선택된 파일: ${file.name}`;
-    Jansori && (Jansori.style.display = "none");
+    if (Jansori) Jansori.style.display = "none";
 
-    selectedFile = file; // 선택한 파일 저장
+    selectedFile = file;
+  } else {
+    imageContainer.innerHTML = "";
+    uploadBtn.textContent = "사진 선택";
+    selectedFile = null;
   }
 });
 
-// 분류 시작 버튼 이벤트 리스너 추가
 classifyBtn.addEventListener("click", async () => {
   if (!selectedFile) {
     alert("먼저 이미지를 업로드하세요!");
     return;
   }
 
-  const formData = new FormData();
-  formData.append("file", selectedFile);
+  const fdata = new FormData();
+  fdata.append("file", selectedFile);
 
   try {
     const response = await fetch("/predict/", {
       method: "POST",
-      body: formData,
+      body: fdata,
     });
 
     if (!response.ok) {
@@ -52,16 +54,15 @@ classifyBtn.addEventListener("click", async () => {
     function sleep(sec) {
       return new Promise((resolve) => setTimeout(resolve, sec * 1000));
     }
+
     resultDiv.innerHTML = `<p>제가 보기에 이 날씨는...</p>`;
     await sleep(2);
-    if (data.weather == "rainy") {
-      resultDiv.innerHTML = `<p>이 사진의 날씨는 <strong>${data.weather}</strong>네요!! 우산챙기세요 지금 당장 !!!!!!!!!!!!!!!!! ☔</p>`;
-    } else if (data.weather) {
+    if (data.weather) {
       resultDiv.innerHTML = `<p>이 사진의 날씨는 <strong>${data.weather}</strong>이네요!! 맞나요???????</p>`;
     } else if (data.error) {
       resultDiv.innerHTML = `<p>에러 발생: ${data.error}</p>`;
     }
   } catch (error) {
-    resultDiv.innerHTML = `<p>에러 발생: ${error.message}</p>`;
+    resultDiv.innerHTML = `<p>에러 발생: ${error.message}</p>`; 
   }
 });
